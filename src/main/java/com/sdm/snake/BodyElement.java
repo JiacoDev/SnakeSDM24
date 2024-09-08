65142b614c620de1ae74c15676495ffa1f33dda6
fpolo_5b8 <fpoloni15@gmail.com>
2024-09-08 17:04:21 +0200

    some minor refactoring

--- src/main/java/com/sdm/snake/BodyElement.java
+++ src/main/java/com/sdm/snake/BodyElement.java
@@ -1,9 +1,4 @@
 package com.sdm.snake;
 
 public record BodyElement(int posX, int posY, BodyType type) {
-    public BodyElement(int posX, int posY, BodyType type) {
-        this.posX = posX;
-        this.posY = posY;
-        this.type = type;
-    }
 }
