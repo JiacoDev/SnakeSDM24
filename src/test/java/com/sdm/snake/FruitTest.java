c486450fb630449c2efe0a7d118368ffc6e3e95f
fpolo_5b8 <fpoloni15@gmail.com>
2024-09-09 12:32:20 +0200

    Creation of Fruit class and related tests

---
+++ src/test/java/com/sdm/snake/FruitTest.java
@@ -0,0 +1,34 @@
+package com.sdm.snake;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FruitTest {
+
+    @Test
+    void testGetPosX() {
+        Fruit fruit = new Fruit(100,100);
+        Assertions.assertEquals(100,fruit.getPosX());
+    }
+
+    @Test
+    void testSetPosX() {
+        Fruit fruit = new Fruit(100,100);
+        fruit.setPosX(50);
+        Assertions.assertEquals(50,fruit.getPosX());
+    }
+
+    @Test
+    void testGetPosY() {
+        Fruit fruit = new Fruit(100,100);
+        Assertions.assertEquals(100,fruit.getPosY());
+    }
+
+    @Test
+    void testSetPosY() {
+        Fruit fruit = new Fruit(100,100);
+        fruit.setPosY(50);
+        Assertions.assertEquals(50,fruit.getPosY());
+
+    }
+}
