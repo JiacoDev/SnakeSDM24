cde4b469dbda2ecceb065fbca591988daa62c3f5
fpolo_5b8 <fpoloni15@gmail.com>
2024-09-09 17:19:18 +0200

    Enhanced SnakeTest by adding a movement test and refactoring all tests for consistency and generalization

---
+++ src/main/java/com/sdm/snake/Board.java
@@ -0,0 +1,19 @@
+package com.sdm.snake;
+
+public class Board {
+    final private int width;
+    final private int height;
+
+    Board(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
--- src/test/java/com/sdm/snake/SnakeTest.java
+++ src/test/java/com/sdm/snake/SnakeTest.java
@@ -9,26 +9,27 @@ import org.junit.jupiter.params.provider.EnumSource;
 
 public class SnakeTest {
 
-    private final int StartingX = 10;
-    private final int StartingY = 10;
+    private final static int STARTING_X = 10;
+    private final static int STARTING_Y = 10;
+    private final static int STARTING_LENGTH = 3;
 
     @Test
     void checkSnakeSize() {
-        Snake snake = new Snake(StartingX,StartingY,10);
-        Assertions.assertEquals(10,snake.getSize());
+    Snake snake = new Snake(STARTING_X, STARTING_Y,STARTING_LENGTH);
+        Assertions.assertEquals(STARTING_LENGTH,snake.getSize());
     }
 
     @Test
     void checkGrowthOfTheSnake() {
-        Snake snake = new Snake();
+        Snake snake = new Snake(STARTING_X, STARTING_Y,STARTING_LENGTH);
         snake.grow();
-        Assertions.assertEquals(2,snake.getSize());
+        Assertions.assertEquals(STARTING_LENGTH+1,snake.getSize());
     }
 
     @ParameterizedTest
     @EnumSource(value = Direction.class) //passing all 4 directions
     void checkGrowthDirection(Direction direction) {
-        Snake snake = new Snake();
+        Snake snake = new Snake(STARTING_X, STARTING_Y,STARTING_LENGTH);
         //you can't change direction from UP to DOWN (the default direction is UP) so I change it to LEFT then DOWN
         if(direction == Direction.DOWN) {
             snake.setDirection(Direction.LEFT);
@@ -36,30 +37,30 @@ public class SnakeTest {
         snake.setDirection(direction);
         snake.grow();
         switch (direction) {
-            case UP ->Assertions.assertEquals(1,snake.getHeadYCoordinate());
-            case DOWN ->Assertions.assertEquals(-1,snake.getHeadYCoordinate());
-            case LEFT ->Assertions.assertEquals(-1,snake.getHeadXCoordinate());
-            case RIGHT ->Assertions.assertEquals(1,snake.getHeadXCoordinate());
+            case UP ->Assertions.assertEquals(11,snake.getHeadYCoordinate());
+            case DOWN ->Assertions.assertEquals(9,snake.getHeadYCoordinate());
+            case LEFT ->Assertions.assertEquals(9,snake.getHeadXCoordinate());
+            case RIGHT ->Assertions.assertEquals(11,snake.getHeadXCoordinate());
         }
 
     }
 
     @Test
     void getXHeadCoordinates() {
-        Snake snake = new Snake();
-        Assertions.assertEquals(0,snake.getHeadXCoordinate());
+        Snake snake = new Snake(STARTING_X, STARTING_Y,STARTING_LENGTH);
+        Assertions.assertEquals(STARTING_X,snake.getHeadXCoordinate());
     }
 
     @Test
     void getYHeadCoordinates() {
-        Snake snake = new Snake();
-        Assertions.assertEquals(0,snake.getHeadYCoordinate());
+        Snake snake = new Snake(STARTING_X, STARTING_Y,STARTING_LENGTH);
+        Assertions.assertEquals(STARTING_Y,snake.getHeadYCoordinate());
     }
 
     @ParameterizedTest
     @EnumSource(value = Direction.class) //passing all 4 directions
     void checkMoveDirection(Direction direction) {
-        Snake snake = new Snake();
+        Snake snake = new Snake(STARTING_X, STARTING_Y,STARTING_LENGTH);
         //you can't change direction from UP to DOWN (the default direction is UP) so I change it to LEFT then DOWN
         if(direction == Direction.DOWN) {
             snake.setDirection(Direction.LEFT);
@@ -67,17 +68,74 @@ public class SnakeTest {
         snake.setDirection(direction);
         snake.move();
         switch (direction) {
-            case UP ->Assertions.assertEquals(1,snake.getHeadYCoordinate());
-            case DOWN ->Assertions.assertEquals(-1,snake.getHeadYCoordinate());
-            case LEFT ->Assertions.assertEquals(-1,snake.getHeadXCoordinate());
-            case RIGHT ->Assertions.assertEquals(1,snake.getHeadXCoordinate());
+            case UP ->Assertions.assertEquals(11,snake.getHeadYCoordinate());
+            case DOWN ->Assertions.assertEquals(9,snake.getHeadYCoordinate());
+            case LEFT ->Assertions.assertEquals(9,snake.getHeadXCoordinate());
+            case RIGHT ->Assertions.assertEquals(11,snake.getHeadXCoordinate());
         }
     }
 
     @Test
     void checkReverseMoveDirection() {
-        Snake snake = new Snake();
+        Snake snake = new Snake(STARTING_X, STARTING_Y,STARTING_LENGTH);
         snake.setDirection(Direction.DOWN);
         Assertions.assertEquals(Direction.UP,snake.getDirection());
+    }
+
+    @Test
+    void checkYHeadMovement(){
+        Snake snake = new Snake(STARTING_X, STARTING_Y,STARTING_LENGTH);
+        for (int i = 0; i < 10; i++) {
+            snake.move();
+        }
+        for (int i = 0; i < 10; i++) {
+            snake.setDirection(Direction.RIGHT);
+            snake.move();
+        }
+        Assertions.assertEquals(STARTING_Y+10,snake.getHeadYCoordinate());
+    }
+
+    @Test
+    void checkXHeadMovement(){
+        Snake snake = new Snake(STARTING_X, STARTING_Y,STARTING_LENGTH);
+        for (int i = 0; i < 10; i++) {
+            snake.move();
+        }
+        for (int i = 0; i < 10; i++) {
+            snake.setDirection(Direction.RIGHT);
+            snake.move();
+        }
+        Assertions.assertEquals(STARTING_X+10,snake.getHeadXCoordinate());
+    }
+
+    @Test
+    void checkXTailMovement(){
+        Snake snake = new Snake(STARTING_X, STARTING_Y,STARTING_LENGTH);
+        for (int i = 0; i < 10; i++) {
+            snake.setDirection(Direction.RIGHT);
+            snake.move();
+        }
+        Assertions.assertEquals(STARTING_X+10-(STARTING_LENGTH-1),snake.getTailXCoordinate(STARTING_LENGTH-1));
+    }
+
+    @Test
+    void checkYTailMovement(){
+        Snake snake = new Snake(STARTING_X, STARTING_Y,STARTING_LENGTH);
+        for (int i = 0; i < 10; i++) {
+            snake.move();
+        }
+        Assertions.assertEquals(STARTING_Y+10-(STARTING_LENGTH-1),snake.getTailYCoordinate(STARTING_LENGTH-1));
+    }
+
+    @Test
+    void getTailXCoordinate() {
+        Snake snake = new Snake(STARTING_X,STARTING_Y,STARTING_LENGTH);
+        Assertions.assertEquals(STARTING_X,snake.getTailXCoordinate(STARTING_LENGTH-1));
+    }
+
+    @Test
+    void getTailYCoordinate() {
+        Snake snake = new Snake(STARTING_X,STARTING_Y,STARTING_LENGTH);
+        Assertions.assertEquals(STARTING_Y-(STARTING_LENGTH-1),snake.getTailYCoordinate(STARTING_LENGTH-1));
     }
 }
