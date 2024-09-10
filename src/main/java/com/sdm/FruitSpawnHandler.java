79a9349300c063789b81802c18485a0cca50676a
fpolo_5b8 <fpoloni15@gmail.com>
2024-09-10 23:11:09 +0200

    fixed randomFruitMove() method

--- src/main/java/com/sdm/FruitSpawnHandler.java
+++ src/main/java/com/sdm/FruitSpawnHandler.java
@@ -3,14 +3,19 @@ package com.sdm;
 import com.sdm.snake.Snake;
 
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class FruitSpawnHandler {
+    
+
     public static void randomFruitMove(Snake snake, Fruit fruit, Board board) {
         Random rand = new Random();
         rand.setSeed(System.currentTimeMillis());
         do {
             fruit.setPosX(rand.nextInt(0,board.getWidth()+1));
             fruit.setPosY(rand.nextInt(0,board.getHeight()+1));
-        } while (CollisionHandler.checkCollision(snake,fruit,board) == GameState.EAT);
+        } while ( IntStream.range(0, snake.getSize()).anyMatch(
+                j -> snake.getTailXCoordinate(j) == fruit.getPosX() &&
+                        snake.getTailYCoordinate(j) == fruit.getPosY()));
     }
 }
