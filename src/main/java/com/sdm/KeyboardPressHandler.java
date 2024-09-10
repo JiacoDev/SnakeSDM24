4ea43e8b502fc9b72f5e5a044362774c7911ec9e
Jiaco <emagiaco21@gmail.com>
2024-09-10 19:19:43 +0200

    Added an EventHandler for KeyEvent that changes snake direction using WASD

---
+++ src/main/java/com/sdm/KeyboardPressHandler.java
@@ -0,0 +1,23 @@
+package com.sdm;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import com.sdm.snake.*;
+
+public class KeyboardPressHandler implements EventHandler<KeyEvent> {
+    private final Snake snake;
+
+    public KeyboardPressHandler(Snake snake) {
+        this.snake = snake;
+    }
+
+    @Override
+    public void handle(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+            case W -> snake.setDirection(Direction.UP);
+            case S -> snake.setDirection(Direction.DOWN);
+            case A -> snake.setDirection(Direction.LEFT);
+            case D -> snake.setDirection(Direction.RIGHT);
+        }
+    }
+}
