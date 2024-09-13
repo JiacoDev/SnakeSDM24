50792726fe0ae6cf2b392fdfdc3caab116f36afc
Nicolo' Davide <nicolodavide1@gmail.com>
2024-09-13 14:44:28 +0200

    Revert "Refactoring Board, from class to record"
    This reverts commit 5796a67c8a81bbff363026a12440c429d301ab94.

--- src/main/java/com/sdm/Board.java
+++ src/main/java/com/sdm/Board.java
@@ -9,20 +9,45 @@ package com.sdm;
  * <p>
  * Getter methods provide access to the width and height of the grid.
  * </p>
- *
- * @param width  The width of the grid.
- * @param height The height of the grid.
  */
-public record Board(int width, int height) {
+public class Board {
+
+    /**
+     * The width of the grid.
+     */
+    private final int width;
+
+    /**
+     * The height of the grid.
+     */
+    private final int height;
 
     /**
      * Constructs a new {@code Board} instance with the specified width and height.
      *
-     * @param width  The width of the grid.
+     * @param width The width of the grid.
      * @param height The height of the grid.
      */
-    public Board {
+    public Board(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * Returns the height of the grid.
+     *
+     * @return The height of the grid.
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Returns the width of the grid.
+     *
+     * @return The width of the grid.
+     */
+    public int getWidth() {
+        return width;
     }
 }
-
-
