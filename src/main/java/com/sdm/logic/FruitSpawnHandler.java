d08ab8e7b751b2ea11a118a097da1af20acff0cf
Jiaco <emagiaco21@gmail.com>
2024-09-16 19:13:00 +0200

    Updated randomFruitMove method to use Position instead of plain coordinates

--- src/main/java/com/sdm/logic/FruitSpawnHandler.java
+++ src/main/java/com/sdm/logic/FruitSpawnHandler.java
@@ -18,8 +18,6 @@ public class FruitSpawnHandler {
     public static void randomFruitMove(Snake snake, Fruit fruit, Board board) {
         do {
             fruit.setPosition(new Position(rand.nextInt(1, board.width()),rand.nextInt(1, board.height())));
-        } while (IntStream.range(0, snake.getSize()).anyMatch(
-                j -> snake.getBody().getBodySegment(j).getX() == fruit.getPosition().getX() &&
-                        snake.getBody().getBodySegment(j).getY() == fruit.getPosition().getY()));
+        } while (IntStream.range(0, snake.getSize()).anyMatch(j ->  snake.getBodySegment(j).equals(fruit.getPosition())));
     }
 }
